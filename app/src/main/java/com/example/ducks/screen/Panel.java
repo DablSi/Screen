84a7d30b59720bc36f0848db3c4e350e0a7d844f
DablSi <ka_nahele@yandex.ru>
2019-02-24 22:53:34 +0300

    Updated bottom navigation panel.

--- app/src/main/java/com/example/ducks/screen/Panel.java
+++ app/src/main/java/com/example/ducks/screen/Panel.java
@@ -11,10 +11,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.*;
+
 
 public class Panel extends Fragment {
+    public void select(ImageButton button, TextView textView){
+        float scale = getResources().getDisplayMetrics().density;
+        button.setPadding((int) (48 * scale + 0.5f),(int) (8 * scale + 0.5f),(int) (48 * scale + 0.5f),(int) (16 * scale + 0.5f));
+        textView.setVisibility(View.VISIBLE);
+    }
+
+    public void unselect(ImageButton button, TextView textView){
+        float scale = getResources().getDisplayMetrics().density;
+        button.setPadding((int) (48 * scale + 0.5f),(int) (16 * scale + 0.5f),(int) (48 * scale + 0.5f),(int) (16 * scale + 0.5f));
+        textView.setVisibility(View.INVISIBLE);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,11 +34,13 @@ public class Panel extends Fragment {
         View view = inflater.inflate(R.layout.fragment_panel, container, false);
 
         // теперь можем получить наши элементы, расположенные во фрагменте
+        TextView textView = view.findViewById(R.id.add);
         ImageButton button = view.findViewById(R.id.first);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!getActivity().getClass().getSimpleName().equals("MainActivity")){
+                select(button, textView);
+                if (!getActivity().getClass().getSimpleName().equals("MainActivity")) {
                     Intent intent1 = new Intent(getContext(), MainActivity.class);
                     startActivity(intent1);
                 }
@@ -36,9 +50,12 @@ public class Panel extends Fragment {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(getActivity().getClass().getSimpleName().equals("MainActivity")){
+                TextView textView1 = view.findViewById(R.id.set);
+                select(button1, textView1);
+                if (getActivity().getClass().getSimpleName().equals("MainActivity")) {
                     Intent intent1 = new Intent(getContext(), Settings.class);
-                    startActivity(intent1);
+                    //startActivity(intent1);
+                    unselect(button, textView);
                 }
             }
         });
