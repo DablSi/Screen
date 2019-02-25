1a2e7d110b8d07f9daf60be12e572c510c0927d8
DablSi <ka_nahele@yandex.ru>
2019-02-25 20:26:44 +0300

    Upgraded the bottom navigation panel

--- app/src/main/java/com/example/ducks/screen/Panel.java
+++ app/src/main/java/com/example/ducks/screen/Panel.java
@@ -15,15 +15,18 @@ import android.widget.*;
 
 
 public class Panel extends Fragment {
-    public void select(ImageButton button, TextView textView){
+    private TextView textView, textView1, textView2;
+    private ImageButton button, button1, button2;
+
+    public void select(ImageButton button, TextView textView) {
         float scale = getResources().getDisplayMetrics().density;
-        button.setPadding((int) (48 * scale + 0.5f),(int) (8 * scale + 0.5f),(int) (48 * scale + 0.5f),(int) (16 * scale + 0.5f));
+        button.setPadding((int) (48 * scale + 0.5f), (int) (8 * scale + 0.5f), (int) (48 * scale + 0.5f), (int) (16 * scale + 0.5f));
         textView.setVisibility(View.VISIBLE);
     }
 
-    public void unselect(ImageButton button, TextView textView){
+    public void unselect(ImageButton button, TextView textView) {
         float scale = getResources().getDisplayMetrics().density;
-        button.setPadding((int) (48 * scale + 0.5f),(int) (16 * scale + 0.5f),(int) (48 * scale + 0.5f),(int) (16 * scale + 0.5f));
+        button.setPadding((int) (48 * scale + 0.5f), (int) (16 * scale + 0.5f), (int) (48 * scale + 0.5f), (int) (16 * scale + 0.5f));
         textView.setVisibility(View.INVISIBLE);
     }
 
@@ -34,8 +37,12 @@ public class Panel extends Fragment {
         View view = inflater.inflate(R.layout.fragment_panel, container, false);
 
         // теперь можем получить наши элементы, расположенные во фрагменте
-        TextView textView = view.findViewById(R.id.add);
-        ImageButton button = view.findViewById(R.id.first);
+        textView = view.findViewById(R.id.add);
+        button = view.findViewById(R.id.first);
+        button1 = view.findViewById(R.id.second);
+        textView1 = view.findViewById(R.id.search);
+        button2 = view.findViewById(R.id.third);
+        textView2 = view.findViewById(R.id.set);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,22 +50,69 @@ public class Panel extends Fragment {
                 if (!getActivity().getClass().getSimpleName().equals("MainActivity")) {
                     Intent intent1 = new Intent(getContext(), MainActivity.class);
                     startActivity(intent1);
+                    unselect(button1, textView1);
+                    unselect(button2, textView2);
                 }
             }
         });
-        ImageButton button1 = view.findViewById(R.id.second);
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textView1 = view.findViewById(R.id.set);
                 select(button1, textView1);
-                if (getActivity().getClass().getSimpleName().equals("MainActivity")) {
-                    Intent intent1 = new Intent(getContext(), Settings.class);
-                    //startActivity(intent1);
+                if (!getActivity().getClass().getSimpleName().equals("Search")) {
+                    Intent intent1 = new Intent(getContext(), Search.class);
+                    startActivity(intent1);
                     unselect(button, textView);
+                    unselect(button2, textView2);
                 }
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                select(button2, textView2);
+                if (!getActivity().getClass().getSimpleName().equals("Settings")) {
+                    Intent intent1 = new Intent(getContext(), Settings.class);
+                    startActivity(intent1);
+                    unselect(button, textView);
+                    unselect(button1, textView1);
+                }
+            }
+        });
+
+        switch (getActivity().getClass().getSimpleName()) {
+            case "MainActivity":
+                select(button, textView);
+                break;
+            case "Search":
+                select(button1, textView1);
+                break;
+            case "Settings":
+                select(button2, textView2);
+        }
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        switch (getActivity().getClass().getSimpleName()) {
+            case "MainActivity":
+                select(button, textView);
+                unselect(button1, textView1);
+                unselect(button2, textView2);
+                break;
+            case "Search":
+                select(button1, textView1);
+                unselect(button, textView);
+                unselect(button2, textView2);
+                break;
+            case "Settings":
+                select(button2, textView2);
+                unselect(button1, textView1);
+                unselect(button, textView);
+        }
     }
 }
